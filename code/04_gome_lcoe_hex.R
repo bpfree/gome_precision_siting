48d6dbdeb22253de3d59732a024a8253cfd44e09
bpfree <brian.patrick.free@gmail.com>
2024-03-19 08:53:12 -0400

    Fix calculation isssues around putting data into hexagonal grid

--- code/04_gome_lcoe_hex.R
+++ code/04_gome_lcoe_hex.R
@@ -47,11 +47,11 @@ pacman::p_load(docxtractr,
 ### Input directories
 lcoe_dir <- "data/a_raw_data/lcoe/GoME_Final_WEA_LCOE_data.gdb"
 
+#### study area grid
+wea_dir <- "data/a_raw_data/gome_wea/Gulf of Maine WEA"
+
 #### hex grid
 gome_gpkg <- "data/b_model_scores/gome_model_scores.gpkg"
-
-
-### Output directories
 
 #####################################
 #####################################
@@ -59,6 +59,11 @@ gome_gpkg <- "data/b_model_scores/gome_model_scores.gpkg"
 # Set parameters
 ## designate region name
 region <- "gome"
+
+# set parameters
+## coordinate reference system
+### EPSG:26918 is NAD83 / UTM 18N (https://epsg.io/26918)
+crs <- "EPSG:26919"
 
 ## layer names
 layer <- "lcoe_2035"
@@ -68,29 +73,25 @@ layer <- "lcoe_2035"
 
 # Load data
 ## hex grid
-gome_hex <- sf::st_read(dsn = gome_gpkg,
-                        layer = sf::st_layers(dsn = gome_gpkg)[[1]][grep(pattern = "final_hex",
-                                                                         x = sf::st_layers(dsn = gome_gpkg)[[1]])])
+gome_hex <- sf::st_read(dsn = gome_gpkg, layer = sf::st_layers(dsn = gome_gpkg)[[1]][grep(pattern = "final_hex",
+                                                                                         x = sf::st_layers(dsn = gome_gpkg)[[1]])])
 
-#####################################
+## study region
+### final WEA
+gome_wea <- sf::st_read(dsn = file.path(wea_dir, "Final_WEA_Poly.shp")) %>%
+  # change projection to match all other data coordinate reference system
+  sf::st_transform(crs = crs)
 
-## Levelized cost of energy (2025)
-lcoe <- sf::st_read(dsn = lcoe_dir,
-                        layer = sf::st_layers(dsn = lcoe_dir)[[1]][grep(pattern = "lcoe",
-                                                                         x = sf::st_layers(dsn = lcoe_dir)[[1]])])
-
+## Levelized cost of energy (2035)
+lcoe <- sf::st_read(dsn = lcoe_dir, layer = sf::st_layers(dsn = lcoe_dir)[[1]][grep(pattern = "lcoe",
+                                                                                    x = sf::st_layers(dsn = lcoe_dir)[[1]])])
 #####################################
 #####################################
 
 # Energy cost in Oregon call areas
 gome_lcoe <- lcoe %>%
-  sf::st_join(x = .,
-              y = gome_hex,
-              join = st_intersects) %>%
-  dplyr::select(OBJECTID, lcoe_2035_GW) %>%
-  # group by the index values as there are duplicates
-  dplyr::group_by(OBJECTID) %>%
-  dplyr::summarise(lcoe_min = min(lcoe_2035_GW))
+  rmapshaper::ms_clip(gome_wea) %>%
+  dplyr::select(lcoe_2035_GW)
 
 #####################################
 
@@ -109,63 +110,49 @@ tmax <- 1.0
 
 ## Values
 ### value minimum
-min <- min(gome_lcoe$lcoe_min)
-# 
-# ### value maximum
-max <- max(gome_lcoe$lcoe_min)
-# 
-# ## Gulf of Maine levelized cost of energy normalized
-# gome_lcoe_2035_norm <- gome_lcoe %>%
-#   dplyr::mutate(lcoe_norm = (narw_min - min) / (max - min),
-#                 layer = "levelized cost of energy (2035)") %>%
-#   # select fields of interest
-#   dplyr::select(OBJECTID,
-#                 layer,
-#                 narw_min,
-#                 lcoe_norm)
+min <- min(gome_lcoe$lcoe_2035_GW)
 
-gome_lcoe_2035_norm <- gome_lcoe %>%
+### value maximum
+max <- max(gome_lcoe$lcoe_2035_GW)
+
+gome_lcoe_norm <- gome_lcoe %>%
   # normalize data between 0.000858 and 1
   dplyr::mutate(lcoe_norm = 
                   # flip the normalization so high costs get low
                   # scores (0.000858) and low costs get higher scores (1.0)
                   (tmax + tmin) - 
                   # normalize the data (will become between 0.8 and 1.0)
-                  (((lcoe_min - min) / (max - min)) *
+                  (((lcoe_2035_GW - min) / (max - min)) *
                      # and then rescale to be between 0.8 and 1.0
                      (tmax - tmin) + tmin)) %>%
-  # create field called "layer" and fill with "levelized cost of energy (2035)" for summary
-  dplyr::mutate(layer = "levelized cost of energy (2035)") %>%
   # select fields of interest
-  dplyr::select(OBJECTID,
-                layer,
-                lcoe_min,
+  dplyr::select(lcoe_2035_GW,
                 lcoe_norm)
 
 #####################################
 #####################################
 
 # # Levelized cost of energy (2035) hex grid
-# gome_hex_lcoe_2025 <- gome_hex[gome_lcoe_2035_norm, ] %>%
-#   # spatially join continental shelf values to Gulf of Maine final WEA hex cells
-#   sf::st_join(x = .,
-#               y = gome_lcoe_2035_norm,
-#               join = st_intersects) %>%
-#   # select fields of importance
-#   dplyr::select(OBJECTID,
-#                 lcoe_norm) %>%
-#   # group by the index values as there are duplicates
-#   dplyr::group_by(OBJECTID) %>%
-#   # summarise the normalized values for the levelized cost of energy in 2035
-#   ## take the minimum value of the index
-#   dplyr::summarise(lcoe_norm_index = min(lcoe_norm))
+gome_lcoe_hex <- gome_hex[gome_lcoe_norm, ] %>%
+  # spatially join continental shelf values to Gulf of Maine final WEA hex cells
+  sf::st_join(x = .,
+              y = gome_lcoe_norm,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(OBJECTID,
+                lcoe_norm) %>%
+  # group by the index values as there are duplicates
+  dplyr::group_by(OBJECTID) %>%
+  # summarise the normalized values for the levelized cost of energy in 2035
+  ## take the minimum value of the index
+  dplyr::summarise(lcoe_norm = min(lcoe_norm))
 
 #####################################
 #####################################
 
 # Export data
 ## Wind submodel
-sf::st_write(obj = gome_lcoe_2035_norm, dsn = gome_gpkg, layer = paste0(region, "_hex_", layer), append = F)
+sf::st_write(obj = gome_lcoe_hex, dsn = gome_gpkg, layer = paste(region, layer, "hex", sep = "_"), append = F)
 
 #####################################
 #####################################
