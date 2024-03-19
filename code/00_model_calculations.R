a334965ee1221ce50a19b6ce526df79b187a7011
bpfree <brian.patrick.free@gmail.com>
2024-03-18 23:24:27 -0400

    Calculate model geometric means

--- code/00_model_calculations.R
+++ code/00_model_calculations.R
@@ -50,30 +50,43 @@ model3 <- 1/3
 #####################################
 
 # set directories
-## submodel raw data directory
-data_dir <- "data/a_raw_suitability/gome_precision_siting.gdb"
-
+## lcoe
 ## Gulf of Maine geopackage (with model scores)
 gome_model_geopackage <- "data/b_model_scores/gome_model_scores.gpkg"
 
+
 #####################################
 #####################################
 
+vms <- sf::st_read(dsn = gome_model_geopackage, layer = sf::st_layers(dsn = gome_model_geopackage)[[1]][grep(pattern = "vms",
+                                                                                 x = sf::st_layers(dsn = gome_model_geopackage)[[1]])])
+narw <- sf::st_read(dsn = gome_model_geopackage, layer = sf::st_layers(dsn = gome_model_geopackage)[[1]][grep(pattern = "narw",
+                                                                           x = sf::st_layers(dsn = gome_model_geopackage)[[1]])])
+lcoe <- sf::st_read(dsn = gome_model_geopackage, layer = sf::st_layers(dsn = gome_model_geopackage)[[1]][grep(pattern = "lcoe",
+                                                                                    x = sf::st_layers(dsn = gome_model_geopackage)[[1]])])
+
+gome_data <- vms %>%
+  cbind(narw,
+        lcoe) %>%
+  dplyr::select(OBJECTID,
+                z_value,
+                narw_min,
+                lcoe_norm) %>%
+  dplyr::rename(vms_min = z_value)
+
 # load data
-gome_data <- sf::st_read(dsn = data_dir, layer = sf::st_layers(dsn = data_dir)[[1]][1]) %>%
-  # remove any unneeded fields -- select all that do not start with "Shape" -- so will exclude "Shape_Length" and "Shape_Area"
-  dplyr::select(!starts_with("Shape")) %>%
+gome_model <- gome_data %>%
   # calculate model scores
   ## model 1: VMS only data
   ## model 2: geometric mean of VMS and North Atlantic right whale
   ## model 3: geometric mean of VMS, North Atlantic right whale, and NREL LCOE data
-  dplyr::mutate(model1 = VMS_2009_2021_Z,
-                model2 = (VMS_2009_2021_Z ** model2) * (NARWden_Z ** model2),
-                model3 = (VMS_2009_2021_Z ** model3) * (NARWden_Z ** model3) * (lcoe_2035_GW_DL ** model3)) %>%
-  dplyr::relocate(Shape, .after = model3)
+  dplyr::mutate(model1 = vms_min,
+                model2 = (vms_min ** model2) * (narw_min ** model2),
+                model3 = (vms_min ** model3) * (narw_min ** model3) * (lcoe_norm ** model3)) %>%
+  dplyr::relocate(geom, .after = model3)
 
 #####################################
 #####################################
 
 # export data
-gome_data <- sf::st_write(obj = gome_data, dsn = gome_model_geopackage, layer = paste(region, "model", sep = "_"), append = F)
+sf::st_write(obj = gome_model, dsn = gome_model_geopackage, layer = paste(region, "model", sep = "_"), append = F)
